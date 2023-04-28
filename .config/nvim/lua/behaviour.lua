73335347297678ebe7d9f41cc78350518d4e431a
Jacob Janzen <jacob.a.s.janzen@gmail.com>
2023-04-28 18:09:58 -0500

    fix macos stuff

--- .config/nvim/lua/behaviour.lua
+++ .config/nvim/lua/behaviour.lua
@@ -3,7 +3,7 @@ vim.opt.tabstop = 4
 vim.opt.expandtab = true
 vim.opt.shiftwidth = 4
 vim.opt.autoindent = true
-vim.opt.directory = '~/.local/share/nvim/swap//'
+vim.opt.directory = 'Users/jjanzen/.local/share/nvim/swap//'
 
 -- matching behaviour
 vim.opt.wildmode = 'longest,list'

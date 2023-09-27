dca266ba070a14dcbbd5146795b5e5348db3df17
Jacob Janzen <jacob.a.s.janzen@gmail.com>
2023-09-27 10:34:33 -0500

    update stuff

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,10 +5,9 @@ alias l 'ls -F'
 alias vim 'nvim'
 alias vi 'nvim'
 alias sl 'ls'
-alias ls 'exa'
+alias ls 'eza'
 alias umvpn 'sudo openconnect --protocol=nc --printcookie vpn.cc.umanitoba.ca/student --user=janzenj2@myumanitoba.ca'
-
-set PATH "$HOME/Library/Python/3.9/bin:/opt/homebrew/opt/grep/libexec/gnubin:$HOME/.cargo/bin:$HOME/.emacs.d/bin:$HOME/.local/bin:$HOME/.npm-global/bin:$PATH"
+set PATH "$HOME/Library/Python/3.9/bin:/opt/homebrew/opt/grep/libexec/gnubin:$HOME/.cargo/bin:$HOME/.emacs.d/bin:$HOME/.local/bin:$HOME/.npm-global/bin:/opt/homebrew/opt/java/bin:/opt/homebrew/opt/sqlite/bin:$PATH"
 set NVM_DIR "$HOME/.nvm"
 set TEXMFCNF "$HOME/.tex_config_dir:"
 source ~/.iterm2_shell_integration.fish
--- .doom.d/init.el
+++ .doom.d/init.el
@@ -56,7 +56,7 @@
        (evil +everywhere); come to the dark side, we have cookies
        file-templates    ; auto-snippets for empty files
        fold              ; (nigh) universal code folding
-       ;;(format +onsave)  ; automated prettiness
+       (format +onsave)  ; automated prettiness
        ;;god               ; run Emacs commands without modifier keys
        ;;lispy             ; vim for lisp, for people who don't like vim
        ;;multiple-cursors  ; editing in many places at once
@@ -96,7 +96,7 @@
        (eval +overlay)     ; run code, run (also, repls)
        ;;gist              ; interacting with github gists
        lookup              ; navigate your code and its documentation
-       ;;lsp               ; M-x vscode
+       lsp               ; M-x vscode
        magit             ; a git porcelain for Emacs
        ;;make              ; run make tasks from Emacs
        ;;pass              ; password manager for nerds
@@ -146,7 +146,7 @@
        javascript        ; all(hope(abandon(ye(who(enter(here))))))
        ;;julia             ; a better, faster MATLAB
        ;;kotlin            ; a better, slicker Java(Script)
-       ;;latex             ; writing papers in Emacs has never been so fun
+       latex             ; writing papers in Emacs has never been so fun
        ;;lean              ; for folks with too much to prove
        ;;ledger            ; be audit you can be
        lua               ; one-based indices? one-based indices
